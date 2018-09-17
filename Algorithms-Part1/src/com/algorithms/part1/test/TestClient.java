b8379ed02a8e584024d59b7df63801ec934d515a
suhastj <suhastj@gmail.com>
2018-09-16 20:08:22 -0400

    Add files via upload
    initial commit with project folder

---
+++ Algorithms-Part1/src/com/algorithms/part1/test/TestClient.java
@@ -0,0 +1,10 @@
+package com.algorithms.part1.test;
+
+public class TestClient {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Algorithms-Part1/src/com/algorithms/part1/uf/bean/UF.java
@@ -0,0 +1,5 @@
+package com.algorithms.part1.uf.bean;
+
+public class UF {
+
+}
